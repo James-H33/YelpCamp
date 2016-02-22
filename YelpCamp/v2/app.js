615de4006ca286be0493e90d72927c74545eba9a
James Hall-Treworgy <James Hall-Treworgy>
2016-02-22 01:49:32 -0500

    Renamed campgrounds.ejs to index.ejs. Also added show.ejs which will act as a detailed page for each new campground.

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -15,17 +15,20 @@ app.set('view engine', 'ejs');
 /////////////////////////////////////////////////
             // Schema Setup //
 /////////////////////////////////////////////////
+
 var campgroundSchema = new mongoose.Schema({
 name: String,
-image: String
+image: String,
+description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 //
 // Campground.create(
 //   {
-//     name: "Bear Ridge",
-//     image: "http://www.photosforclass.com/download/6260705219"
+//     name: "",
+//     image: "http://www.photosforclass.com/download/6260705219",
+//     description: "Dangerous yet beautiful landscape! Beware of leaving food out at night."
 //
 // }, function(err, campground) {
 //       if (err) {
@@ -46,40 +49,57 @@ app.get('/', function(req, res) {
     res.render('landing');
 });
 
+
+// DISPLAY -- Display all Campgrounds
 app.get('/campgrounds', function(req, res) {
   Campground.find({}, function(err, AllCampgrounds){
 if (err){
   console.log(err);
 } else {
-  res.render('campgrounds', {campgroundsVar: AllCampgrounds});
+  res.render('index', {campgroundsVar: AllCampgrounds});
 }
   });
     // res.render('campgrounds', {campgroundsVar: campgrounds});
 });
 
+
+//CREATE -- Create new campgrounds
 app.post('/campgrounds', function(req, res) {
 
     // Get data from /campgrounds/new form and add data to array.
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
 
     // Must push new data in array as an object!
-    var newCampgrounds = {name: name, image: image}
+    var newCampgrounds = {name: name, image: image, description: desc}
     // Create a new campground and save it to the db
     Campground.create(newCampgrounds, function(err, newlyCreated) {
         if(err) {
           console.log(err);
         } else {
-          res.redirect('campgrounds')
+          // Redirect back to /campgrounds
+          res.redirect('/campgrounds');
         }
     });
-    // Redirect back to /campgrounds
-    res.redirect('/campgrounds');
 });
 
+//NEW -- Show form to create new campgrounds
 app.get('/campgrounds/new', function(req, res) {
     // renders new.ejs which contains the form that pushes new data to campgrounds array.
     res.render('new');
+});
+
+//SHOW -- Shows detailed information about specific campground
+app.get('/campgrounds/:id', function(req, res) {
+  // Find campground with ID
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('show', {campground: foundCampground});
+    }
+  });
 });
 
 
